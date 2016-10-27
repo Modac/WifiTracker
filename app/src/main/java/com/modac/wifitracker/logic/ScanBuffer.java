29d41ab68cefbdba90f303b03c776f841c438361
Modac <modac@gmx.net>
2016-10-27 23:37:38 +0200

    Implemented Basic Code of ScanBuffer

--- app/src/main/java/com/modac/wifitracker/logic/ScanBuffer.java
+++ app/src/main/java/com/modac/wifitracker/logic/ScanBuffer.java
@@ -1,9 +1,56 @@
 package com.modac.wifitracker.logic;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Pascal Goldbrunner on 24.10.16.
  *
  */
 
 public class ScanBuffer {
+    private static ScanBuffer instance;
+
+    Map<String, ApBuffer> apBufferMap;
+
+    public ScanBuffer(){
+        apBufferMap = new HashMap<>();
+    }
+
+
+
+
+    public static ScanBuffer getInstance(){
+        if (instance==null)
+            instance = new ScanBuffer();
+        return instance;
+    }
+
+    private class ApBuffer {
+
+        private int lastValue;
+        private int n;
+        private int value;
+
+        private void updateValue(int val){
+            n++;
+            value = (n-1)/n * value + (val/n);
+        }
+
+        private int getValue(){
+            if(n>0) {
+                lastValue = value;
+                n = 0;
+                value = 0;
+            }
+            return lastValue;
+        }
+
+        private void reset(){
+            lastValue=0;
+            n=0;
+            value=0;
+        }
+
+    }
 }
