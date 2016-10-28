e32a1185edb58229e7055656cf4d0790f85ceac2
Modac <modac@gmx.net>
2016-10-28 21:29:12 +0200

    Implemented update-methode of ScanBuffer

--- app/src/main/java/com/modac/wifitracker/logic/ScanBuffer.java
+++ app/src/main/java/com/modac/wifitracker/logic/ScanBuffer.java
@@ -1,5 +1,8 @@
 package com.modac.wifitracker.logic;
 
+import android.net.wifi.ScanResult;
+
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,13 +14,40 @@ import java.util.Map;
 public class ScanBuffer {
     private static ScanBuffer instance;
 
+    // BSSID is used as Key
     Map<String, ApBuffer> apBufferMap;
 
     public ScanBuffer(){
         apBufferMap = new HashMap<>();
     }
 
+    void update(Collection<ScanResult> scanResults) {
 
+        Map<String, Integer> levelMap = new HashMap<>(scanResults.size());
+
+        for (ScanResult scanResult : scanResults) {
+
+            /*if (apBufferMap.containsKey(scanResult.BSSID)) {
+                apBufferMap.get(scanResult.BSSID).updateValue(scanResult.level);
+            }*/
+            levelMap.put(scanResult.BSSID, scanResult.level);
+        }
+
+        for (Map.Entry<String, ApBuffer> mapEntry : apBufferMap.entrySet()){
+            if (levelMap.containsKey(mapEntry.getKey())){
+                mapEntry.getValue().updateValue(levelMap.get(mapEntry.getKey()));
+            } else {
+                mapEntry.getValue().updateValue(0);
+            }
+        }
+
+        for (Map.Entry<String, Integer> mapEntry : levelMap.entrySet()){
+            if(!apBufferMap.containsKey(mapEntry.getKey())){
+                apBufferMap.put(mapEntry.getKey(), new ApBuffer(mapEntry.getValue()));
+            }
+        }
+
+    }
 
 
     public static ScanBuffer getInstance(){
@@ -31,6 +61,15 @@ public class ScanBuffer {
         private int lastValue;
         private int n;
         private int value;
+
+        private ApBuffer(){
+            this.reset();
+        }
+
+        private ApBuffer(int startValue){
+            this();
+            updateValue(startValue);
+        }
 
         private void updateValue(int val){
             n++;
